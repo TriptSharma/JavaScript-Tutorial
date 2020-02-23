860ab864829f8cbb520f594c0630d9fc994a0f0b
TriptSharma <tript35.sharma@gmail.com>
2020-02-23 19:37:03 +0530

    Add JavaScript Demo

---
+++ FancyButton.svelte
@@ -0,0 +1,16 @@
+<!-- https://svelte.dev/examples#dom-event-forwarding -->
+<style>
+	button {
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+		padding: 0.5em 1em;
+		color: royalblue;
+		background: gold;
+		border-radius: 1em;
+		box-shadow: 2px 2px 4px rgba(0,0,0,0.5);
+	}
+</style>
+
+<button on:click>
+	Click me
+</button>
